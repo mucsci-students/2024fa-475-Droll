7bb93cf735e8ab8b80ebd1cd58b98b675949e08c
Valentino A Pirocchi <vapirocc@h4tkt0d5q6x2.muad.local>
2024-11-07 09:31:48 -0500

    music added to battlescene

--- TBD/Assets/Scripts/BattleMusic.cs
+++ TBD/Assets/Scripts/BattleMusic.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class BattleMusic : MonoBehaviour
 {
+    [SerializeField] private AudioClip prelude;
+
+    [SerializeField] private AudioClip battle;
+
+    public bool isPrelude;
+    public float timePassed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isPrelude = true;
+        SoundMusicManager.instance.PlaySoundMusicClip(prelude, transform, 1f);
+    }
+
+    void Update(){
+        timePassed += Time.deltaTime;
+        if(isPrelude){
+            if(timePassed > 40f){
+                isPrelude = false;
+                timePassed = 0;
+                SoundMusicManager.instance.PlaySoundMusicClip(battle, transform, 1f);
+            }
+        }
+        else{
+            if(timePassed > 96f){
+                timePassed = 0;
+                SoundMusicManager.instance.PlaySoundMusicClip(battle, transform, 1f);
+            }
+        }
     }
 }
