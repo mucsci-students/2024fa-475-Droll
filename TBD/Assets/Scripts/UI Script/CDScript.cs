0b54b16a7cd8283014be50e0bf5e16162e36e491
CullenIsntHere <cullen@kurtzpad.com>
2024-11-11 10:43:36 -0500

    CDBox
    Cooldown boxes should be working.

--- TBD/Assets/Scripts/UI Script/CDScript.cs
+++ TBD/Assets/Scripts/UI Script/CDScript.cs
@@ -1,4 +1,4 @@
-/*
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,37 +9,42 @@ public class CDScript : MonoBehaviour
     public float maxTime;
     public float maxScale;
 
-    public GameObject RHObject;
-    public bool RHisOnCD;
-    public float RHcurTime;
-    public float RHcurScale
+    public GameObject CDObject;
+    public bool isOnCD;
+    public float curTime;
+    public float curScale;
+    public KeyCode KeyTest;
     // Start is called before the first frame update
     void Start()
     {
-        maxScale = 10f;
+        maxScale = .5f;
         maxTime = 30f;
-        RHisOnCD = false;
+        isOnCD = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Z) && !RHisOnCD){
-            RHisOnCD = true;
-            RHcurTime = 0f;
+        if(!isOnCD)
+        {
+            CDObject.GetComponent<SpriteRenderer>().sortingOrder = -1;
         }
-        if(RHisOnCD){
-            RHcurTime += Time.deltaTime;
+        if(Input.GetKeyDown(KeyTest) && !isOnCD){
+            isOnCD = true;
+            CDObject.GetComponent<SpriteRenderer>().sortingOrder = 2;
+            curTime = 0f;
+        }
+        if(isOnCD){
+            curTime += Time.deltaTime;
 
-            RHcurScale = ( mathf.Max(0, maxTime - curTime) / maxTime ) * maxScale; 
+            curScale = ( Mathf.Max(0, maxTime - curTime) / maxTime ) * maxScale; 
 
-            RHObject.transform.localeScale = new Vector3(curScale, curScale, 0f);
+            CDObject.transform.localScale = new Vector3(curScale, curScale, 0f);
 
-            if(RHcurTime >= maxTime){
-                RHisOnCD = true;
+            if(curTime >= maxTime){
+                isOnCD = false;
                 curTime = 0;
             }
         }
     }
 }
-*/
