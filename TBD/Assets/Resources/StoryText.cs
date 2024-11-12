c6c32f6bf07366f3c14f015655ddb6fb9f3aa895
vapirocchi <vapirocc@millersville.edu>
2024-11-12 14:09:02 -0500

    storyplot

---
+++ TBD/Assets/Resources/StoryText.cs
@@ -0,0 +1,54 @@
+/*
+
+R = robots
+b = humans
+g = chicken
+
+every text should end with:
+"(press space to continue)"
+peferably on a new line
+
+START SCENE
+r: Human terror shall meet its end.
+b: We created you, we'll destroy you too.
+r: You can fail trying.
+g: BAWK BAAAAAAAWK!
+r: ...
+b: What?
+
+
+
+FIRST BOSS KILL
+g: b.. bawk
+b: Whats up with that chicken.
+r: You humans corrupt everything you touch.
+g: bawk bawk BAWK!
+b: Looks like he is ready for round 2.
+r: Foolish creature.
+
+
+
+SECOND BOSS KILL
+b: That chicken has some real firepower.
+r: Robots were not the only ones to evolve.
+g: BAWK BAWK!
+b: Don't know when to give up do you?
+r: Look who is talking.
+
+
+
+THIRD BOSS KILL
+r: Power depeleting.
+b: We don't have much more supplies.
+b: We need to end this quickly
+r: Affirmative.
+g: VICTORY IS MINE!
+r: ERROR, Experincing... confusion? 
+b: Uhhh, Same here.
+
+
+
+REPEAT
+g: I HAVE BECOME IMORTAL, BOW TO ME!
+
+*/
